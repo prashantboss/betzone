ad5303a0e967f81941b71b246b096e1320250e40
Prashant Trivedi <prashant.boss786@gmail.com>
2021-07-06 22:55:27 +0530

    + remove from player admin

--- resources/views/vendor/multiauth/includes/footer.blade.php
+++ resources/views/vendor/multiauth/includes/footer.blade.php
@@ -218,7 +218,7 @@
         <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
         <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
-        <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+        <!-- <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script> -->
         <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js"></script>
         <!-- others plugins -->
         <script src="{{asset('/')}}admin_assets/js/plugins.js"></script>
